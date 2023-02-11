e3dab4e06cbc70b5c26c53fb08d05dd2f03d8a2b
Elkana Lang'at <elkanahlangatt@gmail.com>
2023-02-11 12:46:39 +0300

    added show cmd

--- console.py
+++ console.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 import cmd
+from models.base_model import BaseModel
+from models import storage
+import inspect
 """
 This program contains the entry point of the command interpreter.
 """
@@ -23,8 +26,47 @@ class HBNBCommand(cmd.Cmd):
         """
         Nothing should be printed on an emptyline.
         """
-        print()
+        pass
+
+    def do_create(self, args):
+        """
+        Creates an instance of a class.
+        """
+        if (args):
+            if args in globals():
+                class_obj = globals().get(args)
+                if class_obj:
+                    new_cls = class_obj()
+                    new_cls.save()
+                    print(new_cls.id)
+            else:
+                print("** class doesn't exist **")
+        else:
+            print("** class name missing **")
     
+    def do_show(self, args):
+        """
+        Prints the string representation of an instance,
+        based on the class name and id
+        """
+        args_list = args.split()
+        args_len = len(args_list)
+        obj_dict = storage.all()
+
+        if args_len == 0:
+            print("** class name missing **")
+        elif args_len == 1:
+            if args_list[0] not in globals():
+                print("** class name doesn't exits **")
+            else:
+                print("** instance id missing **")
+        elif args_len >= 2:
+            if args_list[0] in globals():
+                if f"{args_list[0]}.{args_list[1]}" not in obj_dict:
+                    print("** no instance found **")
+                else:
+                    print(obj_dict[f"{args_list[0]}.{args_list[1]}"])   
+
 
 if __name__ == "__main__":
     HBNBCommand().cmdloop()
