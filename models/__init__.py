93439b56d39f1983b21aa7826cf6354e949c7c6f
Elkana Lang'at <elkanahlangatt@gmail.com>
2023-02-23 14:53:39 +0300

    base redo

--- models/__init__.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-from models.engine.file_storage import FileStorage
-"""
-The file is used to mark a directory as a Python package.
-"""
-
-
-storage = FileStorage()
-storage.reload()
--- models/base_model.py
+++ models/base_model.py
@@ -1,75 +1,40 @@
 #!/usr/bin/python3
 import uuid
 from datetime import datetime
-from models import storage
-"""
-This module defines a class ``BaseModel``.
+"""This module defines all common attributes.
 
-``BaseModel`` defines all common attributes/methods for other classes.
-
+The methods/attributes for other classes start here.
+It defines a class ``BaseModel``
 """
 
 
 class BaseModel:
+    """A base model that has common attribute/methods for other classes.
     """
-    Represents a class ``BaseModel``.
-    """
 
-    def __init__(self, *args, **kwargs):
-        """
-        Initializes the instance of class ``BaseModel``.
-
-        Args:
-            *args (any): variable number of arguments
-            **kwargs (any): variable number of key/value arguments
-        Attributes:
-            id (str): unique ID for the object
-            created_at (any): current datetime when an instance is created.
-            updated_at (any): current datetime when an instance is created,
-                    any it will be updated everytime the object is changed.
-        """
-
-        if kwargs:
-            for key, value in kwargs.items():
-                if key != "__class__":
-                    if key == "created_at" or key == "updated_at":
-                        setattr(self, key, datetime.fromisoformat(value))
-                    else:
-                        setattr(self, key, value)
-        else:
-            self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = self.created_at
-            storage.new(self)
+    def __init__(self):
+        """Initializes the basemodel class."""
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
 
     def __str__(self):
-        """
-        Return a string representation of an object.
-        """
-
-        return f"[{self.__class__.__name__}] ({self.id} {self.__dict__})"
+        """Return the string representation of an object."""
+        return f"{[self.__class__.__name__]} ({self.id}) {self.__dict__}"
 
     def save(self):
-        """
-        Updates the public instance attribute  ``updated_at,
+        """Updates the public instance attribute update_at,
         with the current datetime.
         """
         self.updated_at = datetime.now()
-        storage.save()
 
     def to_dict(self):
-        """
-        Returns a dictionary containing all keys/values of __dict__,
+        """Returns a dictionary containing all keys/values of __dict__,
         of the instance.
-        - by using self.__dict__, only instance attributes set will be returned
-        - a key __class__ must be added to the dict with the class name
-        - created_at and updated_at must be converted to string object
         """
-        get_dict = dict(self.__dict__)
-        get_dict["__class__"] = self.__class__.__name__
+        obj_dict = dict(self.__dict__)
+        obj_dict['__class__'] = self.__class__.__name__
+        obj_dict['created_at'] = self.created_at.isoformat()
+        obj_dict['updated_at'] = self.updated_at.isoformat()
 
-        for key, value in get_dict.items():
-            if key == "created_at" or key == "updated_at":
-                get_dict[key] = value.isoformat()
-
-        return get_dict
+        return obj_dict
